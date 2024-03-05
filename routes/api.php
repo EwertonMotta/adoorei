3e077786832f56e36e31d31ae05fad01ad23c881
Ewerton Motta <contato@ewerton.dev>
2024-03-05 20:32:48 -0300

    defining routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'OK!',
+    ], 200);
+});
+
+Route::controller(App\Http\Controllers\API\ProductController::class)
+    ->group(function () {
+        Route::get('/products', 'index')->name('products.index');
+    });
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->redirectTo('/api');
+});
