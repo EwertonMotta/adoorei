22162995254932624afb7f272d194637433866fa
Ewerton Motta <contato@ewerton.dev>
2024-03-07 12:22:23 -0300

    configuring FormRequests

--- app/Http/Requests/UpdateSaleRequest.php
+++ app/Http/Requests/UpdateSaleRequest.php
@@ -11,7 +11,7 @@ class UpdateSaleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateSaleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'products' => 'required|array',
+            'products.*.product_id' => 'required|exists:products,id',
+            'products.*.quantity' => 'required|integer|min:1',
         ];
     }
 }
