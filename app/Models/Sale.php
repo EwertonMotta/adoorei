47c8eaf39c8885a53d7e08c96e4ccae2fad2d451
Ewerton Motta <contato@ewerton.dev>
2024-03-07 12:23:57 -0300

    correcting and creating methods

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -4,20 +4,37 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Sale extends Model
 {
+    protected $primaryKey = 'id';
+    public $incrementing = false;
     protected $fillable = [
-        'sale_id',
-        'product_id',
-        'quantity',
-        'price'
+        'id',
+        'amount'
     ];
 
-    public function product(): BelongsToMany
+    public function showAmount(): Attribute
     {
-        return $this->belongsToMany(Product::class);
+        return Attribute::make(
+            get: fn ($value, $attributes) => number_format($attributes['amount'], 2, ',', '.'),
+        );
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }
+
+    public static function generateId()
+    {
+        $sale = self::whereDate('created_at', today())->orderBy('id', 'desc')->first();
+        if ($sale) {
+            return $sale->id + 1;
+        }
+        return (int) today()->format('Ymd') . 1;
     }
 }
