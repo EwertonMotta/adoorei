f0e3bb38aed320ff6421aacb89d7ec6674fcaf11
Ewerton Motta <contato@ewerton.dev>
2024-03-08 13:34:21 -0300

    typing ProductController

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(): ResourceCollection
     {
         return ProductResource::collection(Product::all());
     }
