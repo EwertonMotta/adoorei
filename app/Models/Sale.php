851eb92ff35295f49578f38cd2dc1b4496e4adcf
Ewerton Motta <contato@ewerton.dev>
2024-03-08 13:37:33 -0300

    adding factory and typing Sale Model

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -5,22 +5,27 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Sale extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = 'id';
+
     public $incrementing = false;
+
     protected $fillable = [
         'id',
-        'amount'
+        'amount',
     ];
 
     public function showAmount(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, $attributes) => number_format($attributes['amount'], 2, ',', '.'),
+            get: fn (mixed $value, mixed $attributes) => number_format($attributes['amount'], 2, ',', '.'),
         );
     }
 
@@ -29,12 +34,13 @@ class Sale extends Model
         return $this->belongsToMany(Product::class)->withPivot('quantity');
     }
 
-    public static function generateId()
+    public static function generateId(): int|string
     {
         $sale = self::whereDate('created_at', today())->orderBy('id', 'desc')->first();
         if ($sale) {
-            return $sale->id + 1;
+            return (string) $sale->id + 1;
         }
-        return (int) today()->format('Ymd') . 1;
+
+        return today()->format('Ymd'). 1;
     }
 }
